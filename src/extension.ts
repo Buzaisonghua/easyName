b6e5144ff7698095ddfed78a1f1d6e21613281f6
BuZaiSongHua <99548050@qq.com>
2022-05-23 11:51:39 +0800

    token 过期问题修复

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import { Console } from 'console';
 import * as vscode from 'vscode';
 const _ = require('lodash');
 // @ts-ignore
@@ -10,21 +11,26 @@ export function activate(context: vscode.ExtensionContext) {
 		let editor = vscode.window.activeTextEditor;
 		let selection: any = editor?.selection
 		let text = editor?.document.getText(selection)
-		const url = 'https://aip.baidubce.com/rpc/2.0/mt/texttrans/v1?access_token=24.bb76e969627eb1a9cf886c98cfe69f06.2592000.1652968235.282335-26010830'
-		const json = { q: text, from: "zh", to: "en", termIds: '' }
-		request.post(url, {
-			json: json
-		}, (error: any, res: any, body: any) => {
+		request.get('https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=FD7yhYZqMFPBPwMQPM4Tqvwa&client_secret=gb2tmZBHav6MiG7apDnOGGwKSxEW6fs3', {}, (error: any, res: any, body: any) => {
 			if (error) {
 				vscode.window.showInformationMessage('网络错误，请重试');
 				return
 			}
-			console.log(body)
-			let text = body.result.trans_result[0].dst
-			let replaceText = _.camelCase(text);
-			editor?.edit(editorEdit => {
-				// 这里可以做以下操作: 删除, 插入, 替换, 设置换行符
-				editorEdit.replace(selection, replaceText);
+			const url = `https://aip.baidubce.com/rpc/2.0/mt/texttrans/v1?access_token=${JSON.parse(res.body).access_token}`
+			const json = { q: text, from: "zh", to: "en", termIds: '' }
+			request.post(url, {
+				json: json
+			}, (error: any, res: any, body: any) => {
+				if (error) {
+					vscode.window.showInformationMessage('网络错误，请重试');
+					return
+				}
+				let text = body.result.trans_result[0].dst
+				let replaceText = _.camelCase(text);
+				editor?.edit(editorEdit => {
+					// 这里可以做以下操作: 删除, 插入, 替换, 设置换行符
+					editorEdit.replace(selection, replaceText);
+				})
 			})
 		})
 	});
